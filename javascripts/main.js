2938eb2ac38611f6126120808bf20ab286e0a6de
drworm75 <dwgames@gmail.com>
2017-04-09 23:28:20 -0500

    Complete.  Grunt returns no errors

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
-alert('JS Ready');
+
+$("#reveal").on('click', function() {
+	$(this).parent('div').prev('div').find('.hidden').removeClass('hidden');
+	$(this).replaceWith("<p>Callan is a great facilitator, leading the class to the answer without giving us the answer.  Help from Callan is $3.00, or a box of girl scout cookies.</p>");
+	console.log($(this).parent('div'));
+});
